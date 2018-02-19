7e73fca7368b501b5fd837c182c3dea6e0d9707a
arun.r <arun.r@hungama.com>
2018-02-19 14:21:38 +0530

    Added on touch event when clicked on each item in the Recycler View.

--- app/src/main/java/com/example/arunr/retrofithungamaapi/activity/MainActivity.java
+++ app/src/main/java/com/example/arunr/retrofithungamaapi/activity/MainActivity.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.arunr.retrofithungamaapi.R;
 import com.example.arunr.retrofithungamaapi.adapter.MoviesAdapter;
@@ -28,7 +29,7 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private final static String Url = "https://n-pvt.hungama.com/v2/content/movieapp/queue_data.json?device=1080x1920&section_id=1&genre=Gossip&bucket_id=5360&offset=0&user_type=1&version=2.0.10.7&app-id=e3MH8F20cr&limit=10&cp=33682232";
-    List<Movie> movieList = new ArrayList<>();
+    List<Movie> movies = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Decorate the items using various decorators
         RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
-        recyclerView.addItemDecoration(itemDecoration, 17);
+        recyclerView.addItemDecoration(itemDecoration);
 
         ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
 
@@ -49,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
                 // Accessing the appropriate array class in the Json file
-                List<Movie> movies = response.body().getNode().getData();
+                movies = response.body().getNode().getData();
                 recyclerView.setAdapter(new MoviesAdapter(movies, R.layout.list_item_movie, getApplicationContext()));
 //                Log.d(TAG, "Number of movies received: " + movies.size());
             }
@@ -65,7 +66,8 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new ClickListener() {
             @Override
             public void onClick(View view, int position){
-                Movie movie = movieList.get(position);
+                Movie movie = movies.get(position);
+                Toast.makeText(getApplicationContext(), movie.getName() + " is selected!", Toast.LENGTH_SHORT).show();
             }
 
             @Override
